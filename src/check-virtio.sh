d160e0f250c9699d68e282a267ee84237d7ede8b
Well <wuyiwangzi@gmail.com>
2021-10-04 22:17:17 +0800

    增加Parallels支持

--- src/check-virtio.sh
+++ src/check-virtio.sh
@@ -1,5 +1,5 @@
 echo "Checking for VirtIO"
-if (grep -r -q -E "(QEMU|VirtualBox)" /sys/devices/virtual/dmi/id/); then
+if (grep -r -q -E "(QEMU|VirtualBox|Parallels)" /sys/devices/virtual/dmi/id/); then
   echo "VirtIO hypervisor detected"
   exit 0
 else
