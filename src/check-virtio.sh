f3221d2b08abc97c0e9c5b65d63d7fb75b8bb1f2
Well <wuyiwangzi@gmail.com>
2021-10-04 22:42:42 +0800

    测试代码

--- src/check-virtio.sh
+++ src/check-virtio.sh
@@ -1,4 +1,5 @@
 echo "Checking for VirtIO"
+ls -l /sys/devices/virtual/dmi/id/
 if (grep -r -q -E "(QEMU|VirtualBox|Parallels)" /sys/devices/virtual/dmi/id/); then
   echo "VirtIO hypervisor detected"
   exit 0
